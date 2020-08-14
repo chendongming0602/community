ba1d9e876e6348093d1a8480176832c01e9fa1ce
陈栋明 <chendm0602@163.com>
2020-08-14 18:10:14 +0800

    排行

---
+++ community/yl_welore/pages/ranking/ranking.js
@@ -0,0 +1,69 @@
+// yl_welore/pages/ranking/ranking.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        user:{
+            user_head_sculpture:"https://lovers-1300783623.cos.ap-shanghai.myqcloud.com/index/lovers-022746657035097861.jpg",
+            user_nick_name:"大家覅"
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
